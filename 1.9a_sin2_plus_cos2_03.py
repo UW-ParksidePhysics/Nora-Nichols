7a4435fbafa7d027f09e39d77d5ab22cc425d670
noranichols <60672022+noranichols@users.noreply.github.com>
2020-03-05 15:24:47 -0600

    Add files via upload

---
+++ 1.9a_sin2_plus_cos2_03.py
@@ -0,0 +1,4 @@
+from math import sin, cos, pi
+x = pi/4
+v =  sin(x)**2 + cos(x)**2
+print (v)
---
+++ 1.9b_sin2_plus_cos2_03.py
@@ -0,0 +1,5 @@
+v0 = 3 #velocity meters per second
+t = 1 #time in seconds
+a = 2 #acceleration in meters per second squared
+s = v0*t + 0.5*a*t**2
+print (s)
